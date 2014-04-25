56b8cab08172d3c86c6ef0bb919ed7480eaff3aa
z <z@skm.co.id>
2014-04-25 20:17:03 +0700

    Creating getLastDate function

--- tiketkai/parser.py
+++ tiketkai/parser.py
@@ -1,7 +1,29 @@
 from bs4 import BeautifulSoup
 
+def getLastDate(soupObj):
+    objList = None
+    lastdate = None
+
+    comboClass = soupObj.findAll(class_="itScheduleCombox")
+
+    for combo in comboClass:
+        if combo['name'] == 'tanggal':
+            objList = combo
+
+    try:
+        for obj in objList:
+            lastdate = obj.string
+        return lastdate
+    except TypeError as e:
+        print("Webpage structure not found. Quitting...")
+        return None
+
+
 soup = BeautifulSoup(open("tiketkai.html"))
-classtgl = soup.find(class_="itScheduleCombox")
 
-#for tgl in classtgl:
+mydate = getLastDate(soup)
 
+if mydate is None:
+    print("something error. Sorry")
+else:
+    print(mydate)
